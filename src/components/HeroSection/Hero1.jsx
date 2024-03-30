cacb63db77927dfcd30a73f06d13f0d5a67cb4a9
Paul MAZEAU <mazeau.paul@gmail.com>
2024-03-30 22:55:25 +0100

    Optimisation performances

--- src/components/HeroSection/Hero1.jsx
+++ src/components/HeroSection/Hero1.jsx
@@ -6,11 +6,11 @@ import '../../styles/hero/hero1.css';
 
 const HomePage = () => {
   useEffect(() => {
+    const motSuiveur = document.getElementById('cursor-word');
     const handlePointerMove = (e) => {
-      var motSuiveur = document.getElementById('cursor-word');
       motSuiveur.style.left = e.pageX + 'px';
       motSuiveur.style.top = e.pageY + 'px';
-      motSuiveur.style.display = 'block';
+      motSuiveur.classList.add('visible');
     };
 
     document.addEventListener('pointermove', handlePointerMove);
@@ -18,58 +18,70 @@ const HomePage = () => {
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
 
-    const resizeCanvas = () => {
+    function resizeCanvas() {
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
       ctx.fillStyle = "black";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
-    };
+    }
 
     window.addEventListener('resize', resizeCanvas);
     resizeCanvas();
 
     ctx.lineJoin = "round";
     ctx.lineCap = "round";
-    ctx.lineWidth = 200;
+    ctx.lineWidth = 200; // Considérer de réduire si possible pour des performances accrues
     ctx.globalCompositeOperation = "destination-out";
 
     let isDrawing = false;
     let lastX = 0;
     let lastY = 0;
+    let animationFrameId;
 
-    const draw = (e) => {
+    function draw(e) {
       if (!isDrawing) return;
       ctx.beginPath();
       ctx.moveTo(lastX, lastY);
       ctx.lineTo(e.clientX, e.clientY);
       ctx.stroke();
       [lastX, lastY] = [e.clientX, e.clientY];
-    };
+    }
 
-    canvas.addEventListener("pointerdown", (e) => {
+    function startDrawing(e) {
       isDrawing = true;
       [lastX, lastY] = [e.clientX, e.clientY];
-    });
+      draw(e);
+    }
 
-    canvas.addEventListener("pointermove", draw);
-    canvas.addEventListener("pointerup", () => (isDrawing = false));
-    canvas.addEventListener("pointerout", () => (isDrawing = false));
+    function stopDrawing() {
+      isDrawing = false;
+      cancelAnimationFrame(animationFrameId);
+    }
+
+    canvas.addEventListener("pointerdown", startDrawing);
+    canvas.addEventListener("pointermove", (e) => {
+      if (isDrawing) {
+        animationFrameId = requestAnimationFrame(() => draw(e));
+      }
+    });
+    canvas.addEventListener("pointerup", stopDrawing);
+    canvas.addEventListener("pointerout", stopDrawing);
 
     // Cleanup
     return () => {
       document.removeEventListener('pointermove', handlePointerMove);
       window.removeEventListener('resize', resizeCanvas);
-      canvas.removeEventListener("pointerdown", draw);
+      canvas.removeEventListener("pointerdown", startDrawing);
       canvas.removeEventListener("pointermove", draw);
-      canvas.removeEventListener("pointerup", draw);
-      canvas.removeEventListener("pointerout", draw);
+      canvas.removeEventListener("pointerup", stopDrawing);
+      canvas.removeEventListener("pointerout", stopDrawing);
     };
   }, []);
 
   return (
     <HeroContainer title="Hero 1">
-      <div id="cursor-word">CLICK</div>
-      <canvas id="draw"></canvas>
+      <div id="cursor-word" className="cursor-word">DRAW</div>
+      <canvas id='draw'></canvas>
     </HeroContainer>
   );
 };
